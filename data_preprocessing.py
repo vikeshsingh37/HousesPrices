d30a346f3a1419edd0906c564bde861b731bf2dc
Vikesh Singh Baghel <vikeshsingh37@gmail.com>
2019-02-10 01:30:17 +0530

    Improved Data processing and tuned hyper parameters

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,102 +5,187 @@ import numpy as np
 import pandas as pd
 from numpy import nanmedian
 from sklearn.preprocessing import OneHotEncoder
+from scipy.stats import skew
+from scipy.special import boxcox1p
 
 class data_preprocessing:
-	def __init__(self,train_data_file, test_data_file):
-		self.train_data_file = train_data_file
-		self.test_data_file = test_data_file
+    def __init__(self,train_data_file, test_data_file):
+        self.train_data_file = train_data_file
+        self.test_data_file = test_data_file
 
-	def impute_data_using_train_data(self, training_data, new_data, cols_to_impute):
+    def impute_data_using_train_data(self, training_data, new_data, cols_to_impute):
 
-	    # Impute missing numerical columns by median
-	    numeric_cols = training_data.select_dtypes(include = ["float16","float32","float64","int16","int32","int64"]).columns.tolist()
-	    num_cols_for_imputing = list(set(numeric_cols).intersection(cols_to_impute))
-	    if len(num_cols_for_imputing)> 0:
-	        for col in num_cols_for_imputing:
-	            col_median = nanmedian(training_data[col])
-	            new_data[col].replace(np.nan,col_median, inplace = True)
+        # Impute missing numerical columns by median
+        numeric_cols = training_data.select_dtypes(include = ["float16","float32","float64","int16","int32","int64"]).columns.tolist()
+        num_cols_for_imputing = list(set(numeric_cols).intersection(cols_to_impute))
+        if len(num_cols_for_imputing)> 0:
+            for col in num_cols_for_imputing:
+                col_median = nanmedian(training_data[col])
+                new_data[col].replace(np.nan,col_median, inplace = True)
 
-	    # Check categorical remaining missing values with median
-	    cat_cols = training_data.select_dtypes(include = ["category","object"]).columns.tolist()
-	    cat_cols_for_imputing = list(set(cat_cols).intersection(cols_to_impute))
-	    if len(cat_cols_for_imputing)> 0:
-	        for col in cat_cols_for_imputing:
-	            most_frequent = training_data[col].value_counts().axes[0].tolist()[0]
-	            new_data[col].replace(np.nan,most_frequent, inplace = True)
+        # Check categorical remaining missing values with median
+        cat_cols = training_data.select_dtypes(include = ["category","object"]).columns.tolist()
+        cat_cols_for_imputing = list(set(cat_cols).intersection(cols_to_impute))
+        if len(cat_cols_for_imputing)> 0:
+            for col in cat_cols_for_imputing:
+                most_frequent = training_data[col].value_counts().axes[0].tolist()[0]
+                new_data[col].replace(np.nan,most_frequent, inplace = True)
 
-	    return(new_data)
+        return(new_data)
 
-	# Encode labels of categorical columns
-	def encode_label_categorical_cols(self, training_data, new_data, cols_to_encode):
-	    return_df = pd.DataFrame()
-	    for col in cols_to_encode:
-	        one_hot_enc = OneHotEncoder()
-	        one_hot_enc.fit(training_data[col].values.reshape(-1,1))
-	        temp = one_hot_enc.transform(new_data[col].values.reshape(-1,1)).toarray()
-	        dfOneHot = pd.DataFrame(temp, columns = [col+str(int(i)) for i in range(temp.shape[1])])
-	        return_df = pd.concat([return_df,dfOneHot], axis = 1)
-	        new_data = new_data.drop(columns=col, axis = 1)
-	        #new_data = pd.concat([new_data, dfOneHot], axis=1)
-	    new_data = pd.concat([new_data,return_df], axis = 1)
-	    return(new_data)
+    # Encode labels of categorical columns
+    def encode_label_categorical_cols(self, training_data, new_data, cols_to_encode):
+        return_df = pd.DataFrame()
+        for col in cols_to_encode:
+            one_hot_enc = OneHotEncoder(categories='auto')
+            one_hot_enc.fit(training_data[col].values.reshape(-1,1))
+            temp = one_hot_enc.transform(new_data[col].values.reshape(-1,1)).toarray()
+            dfOneHot = pd.DataFrame(temp, columns = [col+str(int(i)) for i in range(temp.shape[1])])
+            return_df = pd.concat([return_df,dfOneHot], axis = 1)
+            new_data = new_data.drop(columns=col, axis = 1)
+            #new_data = pd.concat([new_data, dfOneHot], axis=1)
+        new_data = pd.concat([new_data,return_df], axis = 1)
+        return(new_data)
 
-	def get_processed_data(self):
-		train_data = pd.read_csv(self.train_data_file)
-		test_data = pd.read_csv(self.test_data_file)
-		train_data = train_data.sample(frac=1).reset_index(drop=True)
-		X = train_data.drop(columns = ["SalePrice","Id"])
-		y = train_data['SalePrice']
+    def get_processed_data(self):
+        train_data = pd.read_csv(self.train_data_file)
+        test_data = pd.read_csv(self.test_data_file)
+        train_data = train_data.sample(frac=1).reset_index(drop=True)
+        X = train_data.drop(columns = ["SalePrice","Id"])
+        y = train_data['SalePrice']
 
-		# Find columns with missing values
-		missing_value_columns = X.isnull().sum(axis = 0)
-		good_columns = missing_value_columns[missing_value_columns == 0]
-		missing_value_columns =  missing_value_columns[missing_value_columns > 0]
-		miss_cols_precent = 100*missing_value_columns/X.shape[0]
-
-		# High missing values are spread all over the neighborhood. So, removing them from feature selection. Rest may be imputed
-		# Details on reaching this conclusion can be found in random_forest_with_boruta.py
-		remaining_missing_value_columns = miss_cols_precent[miss_cols_precent < 40]
-		remaining_missing_value_columns = remaining_missing_value_columns.axes[0].tolist()
-		cols_to_keep = good_columns.axes[0].tolist() + remaining_missing_value_columns
-		X = X[cols_to_keep]
-
-		# Check the frequencies of character column. If the frequencies are close to number of training rows or there is huge imbalance
-		# in categories these will be of no use in model building
-		# Details on it can be found in random_forest_with_boruta.py
-		cat_cols = X.select_dtypes(include = ["category","object"]).columns.tolist()
-
-		# Columns to remove with high imbalance in categories
-		high_imb_cats = ['RoofMatl', 'Condition2', 'Utilities', 'Street']
-		X.drop(columns = high_imb_cats, axis = 1, inplace = True)
+        # Find columns with missing values
+        missing_value_columns = X.isnull().sum(axis = 0)
+        good_columns = missing_value_columns[missing_value_columns == 0]
+        missing_value_columns =  missing_value_columns[missing_value_columns > 0]
+        miss_cols_precent = 100*missing_value_columns/X.shape[0]
 
 
-		cat_cols = [i for i in cat_cols if i not in high_imb_cats]
+        # High missing values are spread all over the neighborhood. So, removing them from feature selection. Rest may be imputed
+        # Details on reaching this conclusion can be found in random_forest_with_boruta.py
+        remaining_missing_value_columns = miss_cols_precent[miss_cols_precent < 40]
+        remaining_missing_value_columns = remaining_missing_value_columns.axes[0].tolist()
+        cols_to_keep = good_columns.axes[0].tolist() + remaining_missing_value_columns
+        
+        #These columns actually have NA values. So, these NAs shall be treated as NotAvailable
+        cols_with_actual_NA = ['Alley', 'FireplaceQu','PoolQC' , 'Fence' ,'MiscFeature','BsmtQual','BsmtCond','BsmtExposure','FireplaceQu',
+                              'GarageQual','GarageCond','PoolQC']
+        cols_to_keep = list(set(cols_to_keep + cols_with_actual_NA))
+        X = X[cols_to_keep]
+        X[cols_with_actual_NA] = X[cols_with_actual_NA].fillna('NotAvailable')
+        
+        # These are actually categorical columns
+        # 'MSSubClass' had some new categories that are absent in training. So, a hack here :)        
+        #psuedo_num_cols = ['MSSubClass','OverallQual','OverallCond','YrSold','MoSold', 'YearRemodAdd','YearBuilt','GarageYrBlt']
+        psuedo_num_cols = ['MSSubClass','OverallQual','OverallCond','GarageYrBlt']
+        X[psuedo_num_cols] =X[psuedo_num_cols].fillna(X[psuedo_num_cols].median()[0])
+        X['MSSubClass'] = X['MSSubClass'].map({20:1,30:2,40:3,45:4,50:5,60:6,70:7,75:8,80:9,85:10,90:11,120:12,150:13,160:14,180:15,190:16})  
+        X['OverallQual'] = X['OverallQual'].map({10:10,9:9,8:8,7:7,6:6,5:5,4:4,3:3,2:2,1:1})  
+        X['OverallCond'] = X['OverallCond'].map({10:10,9:9,8:8,7:7,6:6,5:5,4:4,3:3,2:2,1:1})
+        #GarageYrBlt has NA values. Imputing by mode
+        X['GarageYrBlt'] =X['GarageYrBlt'].fillna(X['GarageYrBlt'].mode()[0])
+        #X[psuedo_num_cols] = X[psuedo_num_cols].applymap(str) 
+        
+        
+        # Check the frequencies of character column. If the frequencies are close to number of training rows or there is huge imbalance
+        # in categories these will be of no use in model building
+        # Details on it can be found in random_forest_with_boruta.py
+        cat_cols = X.select_dtypes(include = ["category","object"]).columns.tolist()
+    
+        # Columns to remove with high imbalance in categories
+        high_imb_cats = ['RoofMatl', 'Condition2', 'Utilities', 'Street']
+        X.drop(columns = high_imb_cats, axis = 1, inplace = True)
 
-		# Impute missing numerical columns by median and categorical by most frequent
-		X_train = X
-		X = self.impute_data_using_train_data(X_train,X,remaining_missing_value_columns)
 
-		# Check if still there are missing values
-		nan_check = X[remaining_missing_value_columns].isnull().values.any()
+        cat_cols = [i for i in cat_cols if i not in high_imb_cats]
 
-		if(nan_check):
-		    print("There are still NA values")
-		    sys.exit()
+        # Impute missing numerical columns by median and categorical by most frequent
+        X_train = X
+        remaining_missing_value_columns.remove('GarageYrBlt')
+        X = self.impute_data_using_train_data(X_train,X,remaining_missing_value_columns)
 
-		# Convert string columns to one hot encoded vector
-		X = self.encode_label_categorical_cols(training_data = X_train, new_data = X, cols_to_encode=cat_cols)
+        # Check if still there are missing values
+        nan_check = X[remaining_missing_value_columns].isnull().values.any()
 
-		# Prepare test data for predictions
-		columnsTitles = train_data.columns.tolist()
-		columnsTitles.remove("SalePrice")
-		test_data = test_data[columnsTitles]
-		test_data = test_data.reindex(columns=columnsTitles)
-		X_test = test_data
-		# Impute data from training data
-		X_test = self.impute_data_using_train_data(X_train,X_test,columnsTitles)
+        if(nan_check):
+            print("There are still NA values")
+            sys.exit()
+            
+        numeric_feats = X.dtypes[X.dtypes != "object"].index
 
-		# Encode categorical data
-		X_test = self.encode_label_categorical_cols(training_data = X_train, new_data = X_test, cols_to_encode=cat_cols)
+        # Fix skewed numerical features
+        skewed_feats = X[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+        skew_val = pd.DataFrame({'Skew' :skewed_feats})
+        skew_val = skew_val[abs(skew_val.Skew) > 0.75]
+        skewed_features = skew_val.index
+        lam = 0.15
+        for sf in skewed_features:
+            X[sf] = boxcox1p(X[sf], lam)
+            
+        y = np.log1p(y)
+        
 
-		return(X,X_test,y)
+        # Columns with order significance
+        X['ExterQual'] = X['ExterQual'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X['ExterCond'] = X['ExterCond'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X['BsmtQual'] =  X['BsmtQual'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0})
+        X['BsmtCond'] = X['BsmtCond'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0})
+        X['BsmtExposure'] = X['BsmtExposure'].map({'Gd': 4, 'Av': 3, 'Mn': 2, 'No': 1,'NotAvailable': 0})
+        X['HeatingQC'] = X['HeatingQC'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X['KitchenQual'] = X['KitchenQual'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X['FireplaceQu'] = X['FireplaceQu'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0}) 
+        X['GarageQual'] =  X['GarageQual'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0}) 
+        X['GarageCond'] = X['GarageCond'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0})
+        X['PoolQC'] = X['PoolQC'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'NotAvailable': 0})
+
+        # Columns that need one hot encoding and have no ordering significance
+        cols_to_one_hot_enc = ['MSZoning','Alley','LotShape','LandContour','LotConfig','LandSlope','Neighborhood',
+                               'Condition1','BldgType','HouseStyle','RoofStyle','Exterior1st','Exterior2nd',
+                               'MasVnrType','Foundation','BsmtFinType1','BsmtFinType2','Heating','CentralAir','Electrical',
+                               'Functional','GarageType','GarageFinish','PavedDrive','Fence','MiscFeature','SaleType','SaleCondition'] #+ psuedo_num_cols
+        
+        # Convert string columns to one hot encoded vector
+        X = self.encode_label_categorical_cols(training_data = X_train, new_data = X, cols_to_encode=cols_to_one_hot_enc)
+
+        # Prepare test data for predictions
+        columnsTitles = train_data.columns.tolist()
+        columnsTitles.remove("SalePrice")
+        test_data = test_data[columnsTitles]
+        test_data = test_data.reindex(columns=columnsTitles)
+        X_test = test_data
+        
+        X_test.drop(columns = high_imb_cats, axis = 1, inplace = True)
+        
+        # Data Processing as done with training data
+        X_test[cols_with_actual_NA] = X_test[cols_with_actual_NA].fillna('NotAvailable')
+        X_test[psuedo_num_cols]=X_test[psuedo_num_cols].fillna(X_test[psuedo_num_cols].median()[0])
+        X_test['MSSubClass'] = X_test['MSSubClass'].map({20:1,30:2,40:3,45:4,50:5,60:6,70:7,75:8,80:9,85:10,90:11,120:12,150:13,160:14,180:15,190:16})  
+        X_test['OverallQual'] = X_test['OverallQual'].map({10:10,9:9,8:8,7:7,6:6,5:5,4:4,3:3,2:2,1:1})  
+        X_test['OverallCond'] = X_test['OverallCond'].map({10:10,9:9,8:8,7:7,6:6,5:5,4:4,3:3,2:2,1:1})
+        #X_test['GarageYrBlt'] =X_test['GarageYrBlt'].fillna(X_test['GarageYrBlt'].mode()[0])
+        #X_test[psuedo_num_cols] = X_test[psuedo_num_cols].applymap(str)
+        X_test = self.impute_data_using_train_data(X_train,X_test,columnsTitles)
+        
+        skewed_feats = X_test[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+        skew_val = pd.DataFrame({'Skew' :skewed_feats})
+        skew_val = skew_val[abs(skew_val.Skew) > 0.75]
+        skewed_features = skew_val.index
+        lam = 0.15
+        for sf in skewed_features:
+            X_test[sf] = boxcox1p(X[sf], lam)
+
+        X_test['ExterQual'] = X_test['ExterQual'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X_test['ExterCond'] = X_test['ExterCond'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X_test['BsmtQual'] =  X_test['BsmtQual'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0})
+        X_test['BsmtCond'] = X_test['BsmtCond'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0})
+        X_test['BsmtExposure'] = X_test['BsmtExposure'].map({'Gd': 4, 'Av': 3, 'Mn': 2, 'No': 1,'NotAvailable': 0})
+        X_test['HeatingQC'] = X_test['HeatingQC'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X_test['KitchenQual'] = X_test['KitchenQual'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0})
+        X_test['FireplaceQu'] = X_test['FireplaceQu'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0}) 
+        X_test['GarageQual'] =  X_test['GarageQual'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0}) 
+        X_test['GarageCond'] = X_test['GarageCond'].map({'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1,'NotAvailable': 0})
+        X_test['PoolQC'] = X_test['PoolQC'].map({'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'NotAvailable': 0})
+        # Encode categorical data
+        X_test = self.encode_label_categorical_cols(training_data = X_train, new_data = X_test, cols_to_encode=cols_to_one_hot_enc)
+
+        return(X,X_test,y)
--- get_predictions_from_tree_based_models.py
+++ get_predictions_from_tree_based_models.py
@@ -5,7 +5,7 @@
 
 
 import os
-os.chdir("D:/Kaggle/Housing Prices/")
+os.chdir("D:\Github\HousesPrices")
 import data_preprocessing as dp
 import feature_selection as fs
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor,ExtraTreesRegressor
@@ -13,49 +13,75 @@ import numpy as np
 import xgboost as xgb
 import pandas as pd
 from sklearn.model_selection import cross_validate
+import math
+from sklearn.model_selection import RepeatedKFold
+from sklearn.metrics import mean_squared_log_error
+import lightgbm as lgb
+import xgboost as xgb
+from sklearn.metrics import r2_score
+from sklearn.ensemble import ExtraTreesRegressor
 
 
-# In[ ]:
+# In[2]:
+
+
+def cv_error_for_log_transformed_data(X,y,test_model,cv_folds =5):
+    rkf = RepeatedKFold(n_splits=cv_folds, n_repeats=1, random_state=2652124)
+    err = []
+    for train_index, test_index in rkf.split(X):
+        X_train, X_testing = X.iloc[train_index,:], X.iloc[test_index,:]
+        y_train, y_testing = y[train_index], y[test_index]
+        test_model.fit(X_train,y_train)
+        pred = test_model.predict(X_testing)
+        err.append(np.sqrt(mean_squared_log_error(np.expm1(y_testing), np.expm1(pred))))
+    return(np.mean(err))
+
+
+# In[3]:
 
 
 dp_object = dp.data_preprocessing("D:/Kaggle/Housing Prices/train.csv","D:/Kaggle/Housing Prices/test.csv")
 X,X_test,y = dp_object.get_processed_data()
 
 
-# In[ ]:
+# In[4]:
 
 
 fs_object = fs.feature_selection(X,y)
-selected_features = fs_object.boruta_fs(n_estimators = 'auto', max_iter = 1000, n_jobs = 3)
+selected_features = fs_object.boruta_fs(n_estimators = 'auto', max_iter = 1000, n_jobs = 7)
 
 
-# In[ ]:
+# In[5]:
 
 
-rf_model = RandomForestRegressor(n_estimators=1000, n_jobs= 2,oob_score= True, max_features= 5,random_state=42)
+rf_model = RandomForestRegressor(n_estimators=1000, n_jobs= 7,oob_score= True, random_state=42,max_features=6)
 rf_model.fit(X[selected_features], y.values)
-rf_model.oob_score_
+print(rf_model.oob_score_)
 
 cv_results_rf = cross_validate(rf_model, X[selected_features], y.values, cv=10,scoring = 'neg_mean_squared_log_error',
                          return_train_score=False)
 
--np.mean(cv_results_rf['test_score'].tolist())
+print(np.mean([math.sqrt(-x) for x in cv_results_rf['test_score'].tolist()]))
+
+print(cv_error_for_log_transformed_data(X[selected_features],y,rf_model,cv_folds =10))
 
 
-# In[ ]:
+# In[6]:
 
 
 y_predict = rf_model.predict(X_test[selected_features])
 y_predict_rf = y_predict.tolist()
-X_test["SalePrice"] = y_predict.tolist()
+y_predict_rf = np.expm1(y_predict_rf)
+X_test["SalePrice"] = y_predict_rf
 final_data = X_test[["Id","SalePrice"]]
 final_data.to_csv("D:\\Kaggle\\Housing Prices\\predictions_boruta_rf.csv", index=False)
 
 
-# In[ ]:
+# In[7]:
 
 
-gbm_model = GradientBoostingRegressor(n_estimators=50, max_depth = 4, learning_rate=0.11, random_state=42)
+gbm_model = GradientBoostingRegressor(n_estimators=3000, max_depth = 4, learning_rate=0.01, random_state=42,max_features = 3,
+                                     loss='huber')
 gbm_model.fit(X[selected_features], y.values)
 print(gbm_model.score(X[selected_features],y.values))
 
@@ -63,75 +89,100 @@ from sklearn.model_selection import cross_validate
 cv_results_gbm = cross_validate(gbm_model, X[selected_features], y.values, cv=10,scoring = 'neg_mean_squared_log_error',
                          return_train_score=False)
 
--np.mean(cv_results_gbm['test_score'].tolist())
+print(np.mean([math.sqrt(-x) for x in cv_results_gbm['test_score'].tolist()]))
+print(cv_error_for_log_transformed_data(X[selected_features],y,gbm_model,cv_folds =10))
 
 
-# In[ ]:
+# In[8]:
 
 
 y_predict = gbm_model.predict(X_test[selected_features])
-y_predict_gbm = y_predict.tolist()
-X_test["SalePrice"] = y_predict.tolist()
+y_predict_gbm = np.expm1(y_predict.tolist())
+X_test["SalePrice"] = np.expm1(y_predict.tolist())
 final_data = X_test[["Id","SalePrice"]]
 final_data.to_csv("D:\\Kaggle\\Housing Prices\\predictions_boruta_gbm.csv", index=False)
 
 
-# In[ ]:
+# In[9]:
 
 
-import xgboost as xgb
-from sklearn.metrics import r2_score
-xgb_model = xgb.XGBRegressor(objective="reg:linear", random_state=42,max_depth=4, learning_rate=0.11, n_estimators=50,
-                            reg_alpha=0, reg_lambda=0)
+xgb_model = xgb.XGBRegressor(objective="reg:linear", n_estimators=3000, max_depth = 4, learning_rate=0.01, random_state=42,
+                             reg_alpha=0, reg_lambda=0,n_jobs=4,colsample_bytree = 0.3)
 
 xgb_model.fit(X[selected_features], y.values)
 
 cv_results_xgb = cross_validate(xgb_model, X[selected_features], y.values, cv=10,scoring = 'neg_mean_squared_log_error',
                          return_train_score=False)
--np.mean(cv_results_xgb['test_score'].tolist())
+print(np.mean([math.sqrt(-x) for x in cv_results_xgb['test_score'].tolist()]))
+print(cv_error_for_log_transformed_data(X[selected_features],y,xgb_model,cv_folds =10))
 
 
-# In[ ]:
+# In[10]:
 
 
 y_predict = xgb_model.predict(X_test[selected_features])
-y_predict_xgb = y_predict.tolist()
-X_test["SalePrice"] = y_predict.tolist()
+y_predict_xgb = np.expm1(y_predict.tolist())
+X_test["SalePrice"] = np.expm1(y_predict.tolist())
 final_data = X_test[["Id","SalePrice"]]
 final_data.to_csv("D:\\Kaggle\\Housing Prices\\predictions_boruta_xgb.csv", index=False)
 
 
-# In[ ]:
+# In[11]:
 
 
-from sklearn.ensemble import ExtraTreesRegressor
-et_model = ExtraTreesRegressor(n_estimators=1000, n_jobs= 2,oob_score= True, max_features= 5,bootstrap= True, random_state=42)
+et_model = ExtraTreesRegressor(n_estimators=1000, n_jobs= 7, max_features= 6, random_state=42)
 et_model.fit(X[selected_features], y.values)
-et_model.oob_score_
+#print(et_model.oob_score_)
 cv_results_et = cross_validate(et_model, X[selected_features], y.values, cv=10,scoring = 'neg_mean_squared_log_error',
                          return_train_score=False)
 
--np.mean(cv_results_et['test_score'].tolist())
+print(np.mean([math.sqrt(-x) for x in cv_results_et['test_score'].tolist()]))
+print(cv_error_for_log_transformed_data(X[selected_features],y,et_model,cv_folds =10))
 
 
-# In[ ]:
+# In[12]:
 
 
 y_predict = et_model.predict(X_test[selected_features])
-y_predict_et = y_predict.tolist()
-X_test["SalePrice"] = y_predict.tolist()
+y_predict_et = np.expm1(y_predict.tolist())
+X_test["SalePrice"] = np.expm1(y_predict.tolist())
 final_data = X_test[["Id","SalePrice"]]
 final_data.to_csv("D:\\Kaggle\\Housing Prices\\predictions_boruta_et.csv", index=False)
 
 
-# In[ ]:
+# In[13]:
+
+
+lgb_model = lgb.LGBMRegressor(n_estimators=3000, learning_rate=0.01, random_state=42,boosting_type = 'gbdt',max_depth= 4,
+                              colsample_bytree = 0.4,reg_alpha=0, reg_lambda=0, n_jobs =7)
+lgb_model.fit(X[selected_features], y.values)
+cv_results_et = cross_validate(lgb_model, X[selected_features], y.values, cv=10,scoring = 'neg_mean_squared_log_error',
+                         return_train_score=False)
+
+print(np.mean([math.sqrt(-x) for x in cv_results_et['test_score'].tolist()]))
+print(cv_error_for_log_transformed_data(X[selected_features],y,lgb_model,cv_folds =10))
+
+
+# In[14]:
+
+
+y_predict = lgb_model.predict(X_test[selected_features])
+y_predict_lgb = np.expm1(y_predict.tolist())
+X_test["SalePrice"] = np.expm1(y_predict.tolist())
+final_data = X_test[["Id","SalePrice"]]
+final_data.to_csv("D:\\Kaggle\\Housing Prices\\predictions_boruta_lgb.csv", index=False)
+
+
+# In[15]:
 
 
 avg_pred_from_all_models = pd.DataFrame({'ID': X_test["Id"]})
-avg_pred_from_all_models["rf"] = y_predict_rf
+#avg_pred_from_all_models["rf"] = y_predict_rf
 avg_pred_from_all_models["gbm"] = y_predict_gbm
 avg_pred_from_all_models["xgb"] = y_predict_xgb
-avg_pred_from_all_models["et"] = y_predict_et
+#avg_pred_from_all_models["et"] = y_predict_et
+avg_pred_from_all_models["lgb"] = y_predict_lgb
 avg_pred_from_all_models['SalePrice'] = avg_pred_from_all_models.drop(columns = "ID").mean(axis=1)
 avg_pred_from_all_models
 avg_pred_from_all_models[["ID","SalePrice"]].to_csv("D:\\Kaggle\\Housing Prices\\predictions_boruta_avg.csv", index=False)
+
